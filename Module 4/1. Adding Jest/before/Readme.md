e14d96762e5b476fa9c68f2c2658b14d2c5e2ccc
James MIllar <jmillar@sailr.co.uk>
2024-04-24 13:18:13 +0100

    Updates

---
+++ Module 4/1. Adding Jest/before/Readme.md
@@ -0,0 +1,15 @@
+### Module 4
+
+In this module were going to explore how we can create integration tests.
+We are mainly focused on testing the authentication and authorization of our api endpoints.
+
+There are 2 demos in this module.
+
+1. Adding Jest (and SuperTest)
+2. Testing Authenticaton
+
+In the first demo we will add our test frameworks and write a simple test against our OAuth2 Token endpoint.  That's the endpoint that returns our dummy token!
+
+As part of this we've made some changes to our docker compose to allow us to run the tests within Docker.
+
+In our second demo, we will write a test against one of our endpoint to check the authentication and authorisation.
---
+++ Module 4/2. Swagger_Postman/course-service/src/swagger.js
@@ -0,0 +1,44 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Course API",
+      version: "1.0.0",
+      description: "A REST API for exposing LMS course data",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api",
+        description: "Local server",
+      },
+      {
+        url: "https://course-service.azurewebsites.net/api",
+        description: "Production server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        Bearer: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{ Bearer: [] }],
+  },
+  apis: ["./models/courseSchema.js", "./routes/courses.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
+  app.get("/swagger.json", (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(specs);
+  });
+};
